606dc0996e4382079bffe9c942af7b989aab8943
Christian Lorenzo <clbmiami2004@gmail.com>
2019-11-20 21:55:46 -0800

    Initial Commit

---
+++ StarsReviewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarsReviewPractice
+//
+//  Created by Lambda_School_Loaner_201 on 11/20/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
